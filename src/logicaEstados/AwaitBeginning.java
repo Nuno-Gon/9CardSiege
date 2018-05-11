ed692ce8a050c2dbb3bcaace48eb2fdc018e0306
Psyk0 <onunvieira@gmail.com>
2018-05-11 01:15:15 +0100

    chupa
    conas

--- src/logicaEstados/AwaitBeginning.java
+++ src/logicaEstados/AwaitBeginning.java
@@ -11,6 +11,7 @@ public class AwaitBeginning extends StateAdapter  {
 	@Override
 	public IStates start(){
 	    gameData.baralhaCartas();
+	    gameData.lCheck(gameData.getTunnel());
 	    return new AwaitCardSelect(dataGame);
 	}
 	
