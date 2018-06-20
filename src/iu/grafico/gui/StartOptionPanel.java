8bad9a8406a742af9cb2a7eef8aa81a3a02c826d
Psyk0 <onunvieira@gmail.com>
2018-06-20 08:31:47 +0100

    k
    k

--- src/iu/grafico/gui/StartOptionPanel.java
+++ src/iu/grafico/gui/StartOptionPanel.java
@@ -1,5 +1,79 @@
 package iu.grafico.gui;
 
-public class StartOptionPanel {
+import iu.grafico.ObservableGame;
+import logicaJogo.estados.AwaitBeginning;
 
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+import java.util.*;
+import javax.imageio.ImageIO;
+import java.io.*;
+
+public class StartOptionPanel extends JPanel implements Observer{
+
+	ObservableGame game;
+	
+	JButton iniciar = new JButton("Iniciar");
+	JButton sair = new JButton("Sair");
+	
+	StartOptionPanel(ObservableGame g) {
+		game = g;
+		game.addObserver(this);
+		
+		setupComponentes();
+		setupLayout();
+		
+		setVisible(game.getState() instanceof AwaitBeginning);
+	}
+	
+	private void setupLayout()
+    {
+        setLayout(new BoxLayout(this,BoxLayout.Y_AXIS));
+        
+        iniciar.setAlignmentX(Component.CENTER_ALIGNMENT);
+        sair.setAlignmentX(Component.CENTER_ALIGNMENT);
+        
+        add(Box.createVerticalGlue());
+        add(iniciar);
+        
+        add(Box.createVerticalStrut(20));
+        add(sair);
+        add(Box.createVerticalStrut(10));
+       
+       
+   
+         validate();
+       
+    }
+     
+     private void setupComponentes(){
+      
+         iniciar.addActionListener(new ActionListener(){
+             @Override
+            public void actionPerformed(ActionEvent ev) {
+               game.ResolveStart();
+               
+            }
+         });
+         
+         sair.addActionListener(new ActionListener(){
+             @Override
+            public void actionPerformed(ActionEvent ev) {
+               System.exit(0); 
+               
+            }
+         });
+         
+         
+     }
+	
+	@Override
+    public void update(Observable o, Object arg) {
+        setVisible(game.getEstado() instanceof AwaitBeginning);
+        revalidate();
+        repaint();
+    }
 }
